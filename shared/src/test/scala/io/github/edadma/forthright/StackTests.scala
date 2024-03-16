3dc88c8a79cb1f284f9da0572100beb21aa42068
Edward A Maxedon Sr <edadma@gmail.com>
2024-03-16 11:25:47 -0400

    unit tests

--- shared/src/test/scala/io/github/edadma/forthright/StackTests.scala
+++ shared/src/test/scala/io/github/edadma/forthright/StackTests.scala
@@ -8,5 +8,6 @@ class StackTests extends AnyFreeSpec with Matchers with Tests {
   "dup" in { stack("n1 dup") shouldBe stack("n1 n1") }
   "rot" in { stack("n1 n2 n3 rot") shouldBe stack("n2 n3 n1") }
   "over" in { stack("n1 n2 over") shouldBe stack("n1 n2 n1") }
+  "swap" in { stack("n1 n2 swap") shouldBe stack("n2 n1") }
 
 }
